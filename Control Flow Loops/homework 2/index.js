80e73441d41e095556d10e9315bf38e3f8ddf7b9
Makata1962 <il.pachulia@gmail.com>
2022-06-02 19:44:58 +0400

    homework 3 is done

--- Control Flow Loops/homework 2/index.js
+++ Control Flow Loops/homework 2/index.js
@@ -1,1 +1,7 @@
 let n = 1000;
+
+let i = 2;
+
+while(n % i > 50) {
+    console.log(`iteration`);
+}
--- Control Flow Loops/homework 3/index.js
+++ Control Flow Loops/homework 3/index.js
@@ -1,9 +1,6 @@
-let rand = Math.floor(Math.random() * 10);
 
-console.log(rand);
-
-// if (rand != 5) {
-//   console.log("not 5");
-// } else {
-//   console.log(`it's five :D`);
-// }
+let i = 5;
+while (i !== rand) {
+  var rand = Math.floor(Math.random() * 10);
+  console.log(`Not 5`);
+}
