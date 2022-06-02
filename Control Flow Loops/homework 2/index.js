4e695fd0bc932a9968100b90e193ba5732d6fc1b
Makata1962 <il.pachulia@gmail.com>
2022-06-02 23:32:18 +0400

    variable num is defined

--- Control Flow Loops/homework 2/index.js
+++ Control Flow Loops/homework 2/index.js
@@ -1,10 +1,11 @@
 let n = 1000;
-let count = 0;
+let num = 0;
+var result = n / 2;
 
 // while (result / 2 > 50) {
 //   count++;
-//   var result = n / 2;
 //   result++;
+//   console.log(result);
 // }
 
 function divideByTwo(n) {
@@ -15,12 +16,12 @@ function countIteration(n) {
   divideByTwo(n);
   do {
     divideByTwo(result);
-    count++;
+    num++;
     result++;
   } while (result > 50);
 
-  return count;
+  return num;
 }
 
 countIteration(1000);
-console.log(count);
+console.log(num);
