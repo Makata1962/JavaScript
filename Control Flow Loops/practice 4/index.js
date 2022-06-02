bafd75c680b1224f2e997cff48d4f2d90f142756
Makata1962 <il.pachulia@gmail.com>
2022-06-02 16:41:42 +0400

    practice 4 isdone

---
+++ Control Flow Loops/practice 4/index.js
@@ -0,0 +1,25 @@
+const a = 3;
+
+if (a == 0) {
+  console.log(0);
+}
+if (a == 1) {
+  console.log(1);
+}
+
+if (a == 2 || a == 3) {
+  console.log("2 or 3");
+}
+
+switch (a) {
+  case 0:
+    console.log(0);
+    break;
+  case 1:
+    console.log(1);
+    break;
+  case 2:
+  case 3:
+    console.log("2 or 3");
+    break;
+}
