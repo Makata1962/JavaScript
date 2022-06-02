67d841b381ceebc5988f0c0702296edbf04484ea
Makata1962 <il.pachulia@gmail.com>
2022-06-02 16:13:46 +0400

    practice 2 is done

---
+++ Control Flow Loops/practice 2/index.js
@@ -0,0 +1,19 @@
+// let message;
+// let login = "Owner";
+// let login2 = ""
+
+// if (login == "Pitter") {
+//   message = "Hi";
+// } else if (login == "Owner") {
+//   message = "Hello";
+// } else if (login == "") {
+//   message = "unknown";
+// } else {
+//   message = "";
+// }
+
+login = "";
+
+let message =
+  login === "Pitter" ? "Hi" : login === "Owner" ? "Hello" : "unkown";
+console.log(message);
