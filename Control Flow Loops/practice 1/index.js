1fdb57db3cd99ba4073d72340c0dfe966d2e04a5
Makata1962 <il.pachulia@gmail.com>
2022-06-02 15:45:41 +0400

    practice 1 is done

--- Control Flow Loops/practice 1/index.js
+++ Control Flow Loops/practice 1/index.js
@@ -1,9 +1,12 @@
 // let result;
-// if (a + b < 5) {
+// if (a + b < 4) {
 //   result = true;
 // } else {
 //   result = false;
 // }
+a = 5;
+b = 6;
 
-let result = (a + b < 5) ? true : false;
+let result = a + b < 4 ? true : false;
 
+console.log(result);
