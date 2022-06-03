ddb12056e41344f8408234f8ed560e37f5b86d7f
Makata1962 <il.pachulia@gmail.com>
2022-06-03 12:32:31 +0400

    homework 2 is done with much easier way

--- Control Flow Loops/homework 2/index.js
+++ Control Flow Loops/homework 2/index.js
@@ -1,27 +1,9 @@
 let n = 1000;
+let result = n;
 let num = 0;
-var result = n / 2;
 
-// while (result / 2 > 50) {
-//   count++;
-//   result++;
-//   console.log(result);
-// }
-
-function divideByTwo(n) {
-  return (result = n / 2);
+while (result / 2 > 50) {
+  result /= 2;
+  num++;
 }
-
-function countIteration(n) {
-  divideByTwo(n);
-  do {
-    divideByTwo(result);
-    num++;
-    result++;
-  } while (result > 50);
-
-  return num;
-}
-
-countIteration(1000);
 console.log(num);
