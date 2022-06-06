d62b022922ec05b69336995c0b20aaaf0bb896a8
Makata1962 <il.pachulia@gmail.com>
2022-06-07 03:20:01 +0400

    validation is added

--- Functions/task 1/index.js
+++ Functions/task 1/index.js
@@ -1,12 +1,25 @@
 const arr = [];
-function getDivisors(num) {
-  let i = 1;
-  while (i <= num) {
-    if (num / i === Math.floor(num / i)) {
-      arr.push(i);
+try {
+  function getDivisors(num) {
+    if (typeof num !== "number") {
+      throw new TypeError("parameter type is not a Number");
     }
-    i++;
+    if (num <= 0) {
+      throw new Error("number must be positive and more than 0");
+    }
+    let i = 1;
+    while (i <= num) {
+      if (num / i === Math.floor(num / i)) {
+        arr.push(i);
+      }
+      i++;
+    }
   }
+} catch (err) {
+  console.log(err.message);
 }
-getDivisors(12);
+getDivisors("st");
+getDivisors(0);
+getDivisors(16);
+getDivisors(-1);
 console.log(arr);
