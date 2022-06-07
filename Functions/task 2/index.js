0edae21e7ef8366ee5160c30a50cc7e3745572a3
Makata1962 <il.pachulia@gmail.com>
2022-06-07 05:07:20 +0400

    conditions needs correcting

--- Functions/task 2/index.js
+++ Functions/task 2/index.js
@@ -1,8 +1,23 @@
 function f(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    return arr[i];
-    console.log(arr[i]);
+  try {
+    if (arr.isArray) {
+      console.log(!arr.isArray);
+      throw new TypeError("Passed parameter has to be array");
+    }
+
+    if (arr.length < 0) {
+      throw new Error("Passed array can not be empty");
+    }
+    if (typeof arr.every !== "number") {
+      console.log(arr.every);
+      throw new Error("Passed array can only contain numbers");
+    }
+    for (let i = 0; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 }
-
-f([1, 2, 3, 4, 5, 7]);
+const arr = [1, 2, 3];
+f(arr);
--- Functions/task 3/index.js
+++ Functions/task 3/index.js
@@ -1,8 +1,16 @@
 function func() {
   console.log("Hello World!");
 }
-function genericFunction(func) {
 
+function genericFunction(func) {
+  try {
+    if (typeof func() !== "function") {
+      console.log(typeof func());
+      throw new Error("Type of parameter should be function");
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
 genericFunction(func());
