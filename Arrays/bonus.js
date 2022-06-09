83696325a3bf6ec2af1e53deba19c69a5cc5434e
Makata1962 <il.pachulia@gmail.com>
2022-06-10 01:49:15 +0400

    blueprint of bonus task is done

---
+++ Arrays/bonus.js
@@ -0,0 +1,28 @@
+function rotate(arr, num, str) {
+  try {
+    if (
+      Array.isArray(arr) &&
+      typeof num === "number" &&
+      typeof str === "string"
+    ) {
+      for (let i = 0; i < num; i++) {
+        if (str === "right") {
+          let a = arr.pop();
+          arr.unshift(a);
+          console.log(arr);
+        } else if (str === "left") {
+          let a = arr.shift();
+          arr.push(a);
+          console.log(arr);
+        }
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+var arr = [1, 2, 3];
+
+rotate(arr, 1, "left");
+rotate(arr, 1, "right");
