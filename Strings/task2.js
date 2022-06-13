a8461c6c33c5bc73dbd17ad989be3e1b77cd7dcd
Makata1962 <il.pachulia@gmail.com>
2022-06-14 00:43:38 +0400

    task2 is done

--- Strings/task2.js
+++ Strings/task2.js
@@ -1,3 +1,28 @@
-function reverseCase(sentence) {
-    
-}
+const isUpperCase = (char) =>
+  char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90;
+const isLowerCase = (char) =>
+  char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122;
+
+const reverseCase = (sentence) => {
+  if (typeof sentence !== "string") {
+    throw new TypeError("Parameter should be a string");
+  }
+  let newStr = "";
+  const margin = 32;
+  for (let i = 0; i < sentence.length; i++) {
+    const curr = sentence[i];
+    if (isLowerCase(curr)) {
+      newStr += String.fromCharCode(curr.charCodeAt(0) - margin);
+    } else if (isUpperCase(curr)) {
+      newStr += String.fromCharCode(curr.charCodeAt(0) + margin);
+    } else {
+      newStr += curr;
+    }
+  }
+  console.log(newStr);
+  return newStr;
+};
+
+reverseCase("Hello theRe");
+reverseCase("BIG LETTERS");
+reverseCase(12);
