1a2b9b5f00bfaa202e09d927ec5c3a5c0da3147b
Makata1962 <il.pachulia@gmail.com>
2022-06-17 16:28:55 +0400

    task.js is done

--- Objects2/task.js
+++ Objects2/task.js
@@ -1,14 +1,44 @@
-const data = {
-  name: "fred",
-  age: 10,
-  contact: {
-    email: "moo@example.com",
-    meta: {
-      verified: true,
-      tags: ["important"],
-    },
+Object.defineProperty(Object.prototype, "mergeDeepRight", {
+  value: function mergeDeepRight(obj) {
+    let propArray = Object.getOwnPropertyNames(obj);
+    for (let i = 0; i < propArray.length; i++) {
+      if (typeof this[propArray[i]] !== 'object') {
+        this[propArray[i]] = obj[propArray[i]];
+      } else if (Array.isArray(this[propArray[i]])) {
+        this[propArray[i]] = this[propArray[i]].concat(obj[propArray[i]]);
+      } else {
+        this[propArray[i]].mergeDeepRight(obj[propArray[i]])
+      }
+    }
   },
+  enumerable: false,
+});
+
+
+const data = {
+    name: 'fred',
+    age: 10,
+    contact: {
+        email: 'moo@example.com',
+        meta: {
+            verified: true,
+            tags: ['important']
+        }
+    }
 };
+
+
+
+data.mergeDeepRight({
+    age: 40,
+    contact: {
+        email: 'baa@example.com',
+        favorite: true,
+        meta: {
+            tags: ['vip']
+        }
+    }
+});
 
 // data will be
 // {
@@ -24,20 +54,5 @@ const data = {
 // 	}
 // }
 
-Object.prototype.mergeDeepRight = function (...args) {
-  const names = Object.getOwnPropertyNames(data);
-  console.log(names);
-};
 
-const newData = data.mergeDeepRight({
-  age: 40,
-  contact: {
-    email: "baa@example.com",
-    favorite: true,
-    meta: {
-      tags: ["vip"],
-    },
-  },
-});
-
-console.log(newData);
+console.log(data);
